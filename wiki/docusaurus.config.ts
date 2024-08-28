e3f4fc9d3f15ae86cdd5bbaa45dcdad289ad362f
ArvidSilverlock <ArvidSilverlock@gmail.com>
2024-08-28 16:12:03 +0100

    update `Documentation` link

--- wiki/docusaurus.config.ts
+++ wiki/docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
           items: [
             {
               label: 'Documentation',
-              to: '/docs/intro',
+              to: '/docs/Globals',
             },
           ],
         },
