554567b3e6c04c72b03ef1f49d28935a4efc49d0
regexman1 <124459272+regexman1@users.noreply.github.com>
2025-02-28 23:47:26 +0200

    Update docusaurus.config.ts

--- wiki/docusaurus.config.ts
+++ wiki/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://regexman1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Pilot.LSP/',
+  baseUrl: '/Pilot.lua-Luau-LSP/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
