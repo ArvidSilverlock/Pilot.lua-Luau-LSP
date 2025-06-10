29e48062614e1055275563b5dd6b042e74e20c52
Arvid <Bulbusaur18@gmail.com>
2025-06-10 17:52:56 +0100

    move globals.md out of `/docs/types`

--- wiki/src/pages/index.tsx
+++ wiki/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
 				<div className={styles.buttons}>
 					<Link
 						className="button button--secondary button--lg"
-						to="/types/globals"
+						to="/globals"
 					>
 						Waste of Space Documentation
 					</Link>
